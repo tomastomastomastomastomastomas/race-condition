ffb59220e72b2fed8dbb29251bb4bb10cfa070c2
tomas <tomas.gentile@gm2dev.com>
2024-10-15 10:03:50 -0300

    feat(ticket): add functions that manage the purchase

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 let tickets = 10;
+let users = [];
 
 function getTicket(ticket) {
   return new Promise((resolve) => {
     console.log("Buying a ticket");
     setTimeout(() => {
       if (tickets - ticket > 0) {
-        tickets - ticket;
-        console.log("has bought a ticket");
+        tickets -= ticket;
+        console.log("Has bought a ticket");
       } else {
-        console.log("There are not more tickets");
+        console.log("There are not more tickets for that quantity");
       }
       resolve();
     }, Math.random() * 5000);
   });
+}
+
+function buyTicket(ticket) {
+  users.push(ticket);
+  if (users.length === 1) {
+    buyTickets();
+  }
+}
+
+async function buyTickets() {
+  while (users.length > 0) {
+    await getTicket(users[0]);
+    users.shift();
+  }
 }
